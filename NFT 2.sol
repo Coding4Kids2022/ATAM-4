426f671e9c5a88cb20f9d4dd9e523ea08a65d85e
Timidimmilimmi <110459274+Timidimmilimmi@users.noreply.github.com>
2022-08-02 15:29:15 +0200

    Create NFT 2.sol

---
+++ NFT 2.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/untils/Strings.sol";
+
+contract NFT is ERC721 {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private currentTokenId;
+    string public url;
+
+
+
+    constructor(string memory link)  ERC721("The bored Chicken", "(TBC)") {
+          url = link;
+    }
+              
+    function mint() public {
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(msg.sender, newItemId);
+    }
+    
+     function tokenURI(uint256 tokenID) public view override returns (string memory) {
+         return string(abi.encodePacked(url, Strings.toStrings(tokenID)));
+     }
+}
